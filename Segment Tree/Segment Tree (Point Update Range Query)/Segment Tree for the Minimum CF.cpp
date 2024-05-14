53605aba694cb3c9bfd5ef3a607bf96703cc7a62
its-asif <asifiscoding@gmail.com>
2024-05-14 23:01:40 +0600

    segment tree - 2

--- Segment Tree/Segment Tree (Point Update Range Query)/Segment Tree for the Minimum CF.cpp
+++ Segment Tree/Segment Tree (Point Update Range Query)/Segment Tree for the Minimum CF.cpp
@@ -1,6 +1,6 @@
-// https://codeforces.com/edu/course/2/lesson/4/1/practice/contest/273169/problem/A
+// https://codeforces.com/edu/course/2/lesson/4/1/practice/contest/273169/problem/B
 // Codeforces : ITMO Academy: pilot course
-// Segment Tree for the Sum
+// Segment Tree for the Minimum
 
 
 #include<bits/stdc++.h>
@@ -14,6 +14,7 @@ using namespace std;
 const int N = 1e5 + 9;
 ll arr[N];
 ll t[N*4];
+static const int inf = 1e9;
 
 void build(ll node, ll b, ll e){
 	if(b == e){
@@ -28,7 +29,7 @@ void build(ll node, ll b, ll e){
 	build(l, b, mid);
 	build(r, mid+1, e);
 
-	t[node] = (t[l] + t[r]);
+	t[node] = min(t[l], t[r]);
 }
 
 void upd( ll node, ll b, ll e, ll i, ll x){
@@ -46,18 +47,18 @@ void upd( ll node, ll b, ll e, ll i, ll x){
 	upd( l, b, mid, i, x);
 	upd( r, mid+1, e, i, x);
 
-	t[node] = t[l] + t[r];
+	t[node] = min(t[l], t[r]);
 }
 
 ll query( ll node, ll b, ll e, ll i, ll j){
-	if( b > j or e < i) return 0;
+	if( b > j or e < i) return inf;
 	if( b>=i and e <=j) return t[node];
 
 	ll mid = ( b + e ) / 2;
 	ll l = node * 2;
 	ll r = l + 1;
 
-	return query(l, b, mid, i, j) + query(r, mid+1, e, i, j);
+	return min(query(l, b, mid, i, j), query(r, mid+1, e, i, j));
 }
 
 int main(){
